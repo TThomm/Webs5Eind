4a2d5af8d385755f724689d7330153bae5328ba1
TThomm <tgatheun@avans.nl>
2018-03-28 11:32:22 +0200

    no message

---
+++ authentication/app/models/cafe.js
@@ -0,0 +1,25 @@
+﻿// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var cafeSchema = mongoose.Schema({
+
+    cafe: {
+        icon: String,
+        id: String,
+        name: String,
+        address: String
+    }
+
+});
+
+// methods ======================
+// generating a hash
+
+
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Cafe', cafeSchema);
---
+++ authentication/config/database.js
@@ -0,0 +1,5 @@
+﻿module.exports = {
+
+    'url': 'localhost' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
